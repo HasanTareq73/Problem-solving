89f22cb72bc269e6d5d1a024c336a796206d3684
HasanTareq73 <hasantareq73@gmail.com>
2022-06-29 04:23:32 +0600

    Variable,List

--- hello.py
+++ hello.py
@@ -1,1 +1,106 @@
-print("Hello World")
+
+print ("Hello World")
+
+
+x = 1
+if x == 1:
+
+    print ("X is 1")
+
+# Variables and Types
+myint= 7 #integer
+print(myint)
+
+
+myfloat = 7.32 #Float
+print(myfloat)
+
+mystring= 'Hello' #String
+print(mystring)
+
+mystr= 'Hello World'
+mystr2= "it's my first excecise"
+
+print(mystr, ":", mystr2)
+
+one=1
+two=2
+three= one + two
+print(three)
+
+
+Hello= 'hello'
+World= "worldtest"
+
+helloworld= Hello + " " + World
+print(helloworld)
+
+
+a,b= 1,2
+
+print(a, b)
+
+
+
+
+
+
+
+name= 'Mahadi'
+age=25
+birth=1997.12
+
+
+if name == "Mahadi":
+    print("String: %s" % name)
+if isinstance(age, int) and age == 25:
+    print("intrger: %d" % age)
+if isinstance(birth, float) and birth == 1997.12:
+    print("Float: %f" % birth)
+
+
+
+
+
+#Lists
+mylist = []
+mylist.append(3)
+mylist.append(5)
+mylist.append(4)
+
+# print(mylist[0])
+# print(mylist[1])
+# print(mylist[2])
+
+
+for ba in mylist:
+    print(ba)
+
+
+
+
+
+
+
+
+numbers = []
+string1 = []
+name = ["mahadi", "hasan", "tareq"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+string1.append("hello")
+string1.append("world")
+
+
+
+
+second_name = name[1]
+
+
+
+print(numbers)
+print(string1)
+print(" the second name on the names list is %s" % second_name)
