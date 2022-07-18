1dd7bea2e473fc7e5c8e2a7f8c41fbd07406229f
HasanTareq73 <hasantareq73@gmail.com>
2022-07-18 22:15:48 +0600

    Update topic Operator

---
+++ Operators.py
@@ -0,0 +1,67 @@
+from math import remainder
+
+
+number = 1+2*3/4
+
+print(number)
+
+
+# Reminder
+
+
+remainder = 113 % 3
+
+print(remainder)
+
+# Power Of Multipication
+
+squared = 7 ** 2
+
+print (squared)
+
+
+# Using Operator with string
+
+hello = "Hello" + " " + "World"
+
+print(hello)
+
+
+lotsofhello = "Hello" * 3
+
+print(lotsofhello)
+
+
+# Using Operators with list 
+even_num= [2,4,6,8]
+odd_num= [1,3,5,7,9]
+
+all_number= even_num+odd_num
+
+print(all_number)
+
+
+print([1,2,3] * 3)
+
+
+
+# Operator exercise: 
+
+
+x= object()
+y= object()
+
+
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print(" x list contains %d objects" % len(x_list) )
+print(" y_list conatains %d objects" % len(y_list))
+
+print("big_list contains %d object" % len(big_list))
+
+
+
+
+
