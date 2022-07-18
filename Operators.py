e90b097bac953918f794dbeb7ea65da5f5838ff1
HasanTareq73 <hasantareq73@gmail.com>
2022-07-18 22:20:07 +0600

    Operators

--- Operators.py
+++ Operators.py
@@ -64,4 +64,11 @@ print("big_list contains %d object" % len(big_list))
 
 
 
+if x_list.count(x) == 10 and y_list.count(y)==10:
+    print("almost there")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great")
+
+
+
 
