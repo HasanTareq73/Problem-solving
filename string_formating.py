1bb96b71a7c73a82b675812f16833b7928fc364e
HasanTareq73 <hasantareq73@gmail.com>
2022-07-20 21:41:23 +0600

    String-Formating

---
+++ string_formating.py
@@ -0,0 +1,18 @@
+name = "Jhon"
+print("Hello, %s!" % name)
+
+age = 23
+
+print ("%s is %d years old" % (name, age))
+
+
+
+mylist = [5,9,7]
+
+print("A list: %s " % mylist)
+
+
+data = ("jhon", "Doe", 53.44)
+formating_string= "Hello %s %s. your Current balance is $%s."
+
+print(formating_string % data)
