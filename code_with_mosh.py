0cc0ea492b315dcab5697c79549d2c7e9ed910fa
HasanTareq73 <hasantareq73@gmail.com>
2022-07-21 20:38:00 +0600

    code_with-mosh-upgade

--- code_with_mosh.py
+++ code_with_mosh.py
@@ -1,22 +1,38 @@
-# 1. we check in a patient named john smith.
-# he's 20 years old and is a new patient
+# # 1. we check in a patient named john smith.
+# # he's 20 years old and is a new patient
 
 
-name= "jhon smith"
-age= 20
-is_new = True
+# name= "jhon smith"
+# age= 20
+# is_new = True
 
 
-name = input(" what is your name? ")
+# name = input(" what is your name? ")
 
-color = input('What is your faborite color? ')
+# color = input('What is your faborite color? ')
 
-birth_year= input(" birth year: ")
-print(type (birth_year))
+# birth_year= input(" birth year: ")
+# print(type (birth_year))
 
-age = 2022 - int(birth_year)
+# age = 2022 - int(birth_year)
 
-print(type(age))
+# print(type(age))
 
 
-print ("Hi "+ name+ ". your Age is: %d" % age )
+# print ("Hi "+ name+ ". your Age is: %d" % age )
+
+
+# Ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+
+print("Hello you are %f KG" % weight_kg)
+
+
+
+
+
+
+
+print("Hello Mahadi")
